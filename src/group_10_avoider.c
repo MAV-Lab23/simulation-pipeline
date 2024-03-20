1e10db7e96a45f43a96b997f8db6ad700354a42f
Martin <emailregisterlogin@gmail.com>
2024-03-20 23:54:40 +0100

    Cleaned up paparazzi files significantly

--- src/group_10_avoider.c
+++ src/group_10_avoider.c
@@ -9,20 +9,12 @@
 #define NAV_C // needed to get the nav functions like Inside...
 #include "generated/flight_plan.h"
 
-#define ORANGE_AVOIDER_VERBOSE TRUE
-
-#define PRINT(string,...) fprintf(stderr, "[orange_avoider->%s()] " string,__FUNCTION__ , ##__VA_ARGS__)
-#if ORANGE_AVOIDER_VERBOSE
-#define VERBOSE_PRINT PRINT
-#else
-#define VERBOSE_PRINT(...)
-#endif
+#define PRINT(string,...) fprintf(stderr, "[avoider->%s()] " string,__FUNCTION__ , ##__VA_ARGS__)
 
 static uint8_t moveWaypointForward(uint8_t waypoint, float distanceMeters);
 static uint8_t calculateForwards(struct EnuCoor_i *new_coor, float distanceMeters);
 static uint8_t moveWaypoint(uint8_t waypoint, struct EnuCoor_i *new_coor);
-static uint8_t increase_nav_heading(float incrementDegrees);
-static uint8_t chooseRandomIncrementAvoidance(void);
+static uint8_t setNavHeading(float heading);
 
 enum navigation_state_t {
   SAFE,
@@ -31,48 +23,34 @@ enum navigation_state_t {
   OUT_OF_BOUNDS
 };
 
-// define settings
-float oa_color_count_frac_2 = 0.18f;
+enum navigation_state_t nav_state = SEARCH_FOR_SAFE_HEADING;
 
-// define and initialise global variables
-enum navigation_state_t navigation_state_2 = SEARCH_FOR_SAFE_HEADING;
-int32_t color_count = 0;                // orange color count from color filter for obstacle detection
-int16_t obstacle_free_confidence = 0;   // a measure of how certain we are that the way ahead is safe.
-float heading_increment = 5.f;          // heading angle increment [deg]
-float maxDistance = 2.25;               // max waypoint displacement [m]
+float maxDistance = 2.25; // max waypoint displacement [m]
+float new_heading = 0.f;
 
-const int16_t max_trajectory_confidence_2 = 5; // number of consecutive negative object detections to be sure we are obstacle free
+int32_t obstacle_x = -10000;
+int32_t obstacle_y = -10000;
 
-/*
- * This next section defines an ABI messaging event (http://wiki.paparazziuav.org/wiki/ABI), necessary
- * any time data calculated in another module needs to be accessed. Including the file where this external
- * data is defined is not enough, since modules are executed parallel to each other, at different frequencies,
- * in different threads. The ABI event is triggered every time new data is sent out, and as such the function
- * defined in this file does not need to be explicitly called, only bound in the init function
- */
-#ifndef GROUP_10_VISUAL_DETECTION_ID
-#define GROUP_10_VISUAL_DETECTION_ID ABI_BROADCAST
+int32_t obstacle_free_confidence = 0;
+
+#ifndef GROUP_10_OBSTACLE_DETECTION_ID
+#define GROUP_10_OBSTACLE_DETECTION_ID ABI_BROADCAST
 #endif
-static abi_event color_detection_ev;
-static void color_detection_cb(uint8_t __attribute__((unused)) sender_id,
-                               int16_t __attribute__((unused)) pixel_x, int16_t __attribute__((unused)) pixel_y,
-                               int16_t __attribute__((unused)) pixel_width, int16_t __attribute__((unused)) pixel_height,
-                               int32_t quality, int16_t __attribute__((unused)) extra)
+
+static abi_event obstacle_detection_ev;
+
+static void obstacle_detection_cb(uint8_t __attribute__((unused)) sender_id, int32_t x, int32_t y)
 {
-  color_count = quality;
+  obstacle_x = x;
+  obstacle_y = y;
+
+  // TODO: Add obstacle to grid.
 }
 
-/*
- * Initialisation function, setting the colour filter, random seed and heading_increment
- */
 void group_10_avoider_init(void)
 {
-  // Initialise random values
-  srand(time(NULL));
-  chooseRandomIncrementAvoidance();
-
-  // bind our colorfilter callbacks to receive the color filter outputs
-  AbiBindMsgVISUAL_DETECTION(GROUP_10_VISUAL_DETECTION_ID, &color_detection_ev, color_detection_cb);
+  new_heading = findNewHeading();
+  AbiBindMsgOBSTACLE_POINT_DETECTION(GROUP_10_OBSTACLE_DETECTION_ID, &obstacle_detection_ev, obstacle_detection_cb);
 }
 
 /*
@@ -85,31 +63,24 @@ void group_10_avoider_periodic(void)
     return;
   }
 
-  // compute current color thresholds
-  int32_t color_count_threshold = oa_color_count_frac_2 * front_camera.output_size.w * front_camera.output_size.h;
+  PRINT("obstacle_x: %d, obstacle_y: %d \n", obstacle_x, obstacle_y);
 
-  VERBOSE_PRINT("Color_count: %d  threshold: %d state: %d \n", color_count, color_count_threshold, navigation_state_2);
+  float moveDistance = maxDistance;
 
-  // update our safe confidence using color threshold
-  if(color_count < color_count_threshold){
-    obstacle_free_confidence++;
-  } else {
-    obstacle_free_confidence -= 2;  // be more cautious with positive obstacle detections
-  }
+  // TODO: Check grid here to see if an obstacle is ahead / found.
 
-  // bound obstacle_free_confidence
-  Bound(obstacle_free_confidence, 0, max_trajectory_confidence_2);
+  // TODO: Alter obstacle_free_confidence based on grid output.
+  obstacle_free_confidence = 3;
 
-  float moveDistance = fminf(maxDistance, 0.2f * obstacle_free_confidence);
-
-  switch (navigation_state_2){
+  switch (nav_state) {
     case SAFE:
       // Move waypoint forward
       moveWaypointForward(WP_TRAJECTORY, 1.5f * moveDistance);
-      if (!InsideObstacleZone(WaypointX(WP_TRAJECTORY),WaypointY(WP_TRAJECTORY))){
-        navigation_state_2 = OUT_OF_BOUNDS;
-      } else if (obstacle_free_confidence == 0){
-        navigation_state_2 = OBSTACLE_FOUND;
+
+      if (!InsideObstacleZone(WaypointX(WP_TRAJECTORY),WaypointY(WP_TRAJECTORY))) {
+        nav_state = OUT_OF_BOUNDS;
+      } else if (obstacle_free_confidence == 0) {
+        nav_state = OBSTACLE_FOUND;
       } else {
         moveWaypointForward(WP_GOAL, moveDistance);
       }
@@ -120,33 +91,33 @@ void group_10_avoider_periodic(void)
       waypoint_move_here_2d(WP_GOAL);
       waypoint_move_here_2d(WP_TRAJECTORY);
 
-      // randomly select new search direction
-      chooseRandomIncrementAvoidance();
+      // TODO: Search new direction
+      new_heading = findNewHeading();
 
-      navigation_state_2 = SEARCH_FOR_SAFE_HEADING;
+      nav_state = SEARCH_FOR_SAFE_HEADING;
 
       break;
     case SEARCH_FOR_SAFE_HEADING:
-      increase_nav_heading(heading_increment);
+        setNavHeading(new_heading);
 
       // make sure we have a couple of good readings before declaring the way safe
-      if (obstacle_free_confidence >= 2){
-        navigation_state_2 = SAFE;
+      if (obstacle_free_confidence >= 2) {
+        nav_state = SAFE;
       }
       break;
     case OUT_OF_BOUNDS:
-      increase_nav_heading(heading_increment);
+      setNavHeading(new_heading);
       moveWaypointForward(WP_TRAJECTORY, 1.5f);
 
       if (InsideObstacleZone(WaypointX(WP_TRAJECTORY),WaypointY(WP_TRAJECTORY))){
         // add offset to head back into arena
-        increase_nav_heading(heading_increment);
+        setNavHeading(new_heading);
 
         // reset safe counter
         obstacle_free_confidence = 0;
 
         // ensure direction is safe before continuing
-        navigation_state_2 = SEARCH_FOR_SAFE_HEADING;
+        nav_state = SEARCH_FOR_SAFE_HEADING;
       }
       break;
     default:
@@ -158,17 +129,16 @@ void group_10_avoider_periodic(void)
 /*
  * Increases the NAV heading. Assumes heading is an INT32_ANGLE. It is bound in this function.
  */
-uint8_t increase_nav_heading(float incrementDegrees)
+uint8_t setNavHeading(float heading)
 {
-  float new_heading = stateGetNedToBodyEulers_f()->psi + RadOfDeg(incrementDegrees);
-
+  float new_heading = heading;
   // normalize heading to [-pi, pi]
   FLOAT_ANGLE_NORMALIZE(new_heading);
 
   // set heading, declared in firmwares/rotorcraft/navigation.h
   nav.heading = new_heading;
 
-  VERBOSE_PRINT("Increasing heading to %f\n", DegOfRad(new_heading));
+  PRINT("Setting nav heading to %f\n", DegOfRad(new_heading));
   return false;
 }
 
@@ -188,14 +158,16 @@ uint8_t moveWaypointForward(uint8_t waypoint, float distanceMeters)
  */
 uint8_t calculateForwards(struct EnuCoor_i *new_coor, float distanceMeters)
 {
-  float heading  = stateGetNedToBodyEulers_f()->psi;
+  float heading = stateGetNedToBodyEulers_f()->psi;
 
   // Now determine where to place the waypoint you want to go to
   new_coor->x = stateGetPositionEnu_i()->x + POS_BFP_OF_REAL(sinf(heading) * (distanceMeters));
   new_coor->y = stateGetPositionEnu_i()->y + POS_BFP_OF_REAL(cosf(heading) * (distanceMeters));
-  VERBOSE_PRINT("Calculated %f m forward position. x: %f  y: %f based on pos(%f, %f) and heading(%f)\n", distanceMeters,	
-                POS_FLOAT_OF_BFP(new_coor->x), POS_FLOAT_OF_BFP(new_coor->y),
-                stateGetPositionEnu_f()->x, stateGetPositionEnu_f()->y, DegOfRad(heading));
+
+  PRINT("Calculated %f m forward position. x: %f  y: %f based on pos(%f, %f) and heading(%f)\n", distanceMeters,	
+        POS_FLOAT_OF_BFP(new_coor->x), POS_FLOAT_OF_BFP(new_coor->y),
+        stateGetPositionEnu_f()->x, stateGetPositionEnu_f()->y, DegOfRad(heading));
+
   return false;
 }
 
@@ -204,25 +176,16 @@ uint8_t calculateForwards(struct EnuCoor_i *new_coor, float distanceMeters)
  */
 uint8_t moveWaypoint(uint8_t waypoint, struct EnuCoor_i *new_coor)
 {
-  VERBOSE_PRINT("Moving waypoint %d to x:%f y:%f\n", waypoint, POS_FLOAT_OF_BFP(new_coor->x),
-                POS_FLOAT_OF_BFP(new_coor->y));
+  PRINT("Moving waypoint %d to x:%f y:%f\n", waypoint, POS_FLOAT_OF_BFP(new_coor->x), POS_FLOAT_OF_BFP(new_coor->y));
+
   waypoint_move_xy_i(waypoint, new_coor->x, new_coor->y);
+
   return false;
 }
 
-/*
- * Sets the variable 'heading_increment' randomly positive/negative
- */
-uint8_t chooseRandomIncrementAvoidance(void)
+float findNewHeading()
 {
-  // Randomly choose CW or CCW avoiding direction
-  if (rand() % 2 == 0) {
-    heading_increment = 5.f;
-    VERBOSE_PRINT("Set avoidance increment to: %f\n", heading_increment);
-  } else {
-    heading_increment = -5.f;
-    VERBOSE_PRINT("Set avoidance increment to: %f\n", heading_increment);
-  }
-  return false;
+  // TODO: Implement this to determine which way to go.
+  return 0.f;
 }
 
--- src/group_10_obstacle_detector.h
+++ src/group_10_obstacle_detector.h
@@ -1,11 +1,6 @@
 #ifndef GROUP_10_OBSTACLE_DETECTOR_H
 #define GROUP_10_OBSTACLE_DETECTOR_H
 
-extern float oag_color_count_frac;  // obstacle detection threshold as a fraction of total of image
-extern float oag_floor_count_frac;  // floor detection threshold as a fraction of total of image
-extern float oag_max_speed;         // max flight speed [m/s]
-extern float oag_heading_rate;      // heading rate setpoint [rad/s]
-
 extern void group_10_obstacle_detector_init(void);
 extern void group_10_obstacle_detector_periodic(void);
 
