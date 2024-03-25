fdacbbd269231db9e1a501bec50cf46579f0e7bd
Martin <emailregisterlogin@gmail.com>
2024-03-25 10:54:28 +0100

    Update to work with new system

--- src/group_10_obstacle_detector.c
+++ src/group_10_obstacle_detector.c
@@ -21,44 +21,24 @@
 #define GROUP_10_OBSTACLE_DETECTION_ID 50
 #endif
 
-
 struct image_t *obstacle_detector(struct image_t *img, uint8_t camera_id);
 
-struct image_t *obstacle_detector(struct image_t *img, uint8_t camera_id __attribute__((unused)))
-{
-  /*
-    TODO: PROCESS img with OpenCV
-  */
+struct image_t *obstacle_detector(struct image_t *img, uint8_t camera_id __attribute__((unused))) {
   //PRINT("Got image from drone (width: %d, height: %d) \n", img->w, img->h);
-
 #ifdef GROUP_10_OPENCV
   if (img->type == IMAGE_YUV422) {
+    DroneState state = getDroneState();
     // Call OpenCV (C++ from paparazzi C function)
-    opencv_wrapper((char *) img->buf, img->w, img->h);
-    //PRINT("Ran opencv_wrapper");
+    opencv_wrapper((char*)img->buf, img->w, img->h, state);
   }
 #endif
-  
-  DroneState drone_state = getDroneState();
-
-  // TODO: Determine obstacle coordinates and set them here.
-  float x = drone_state.optitrack_pos.x;
-  float y = drone_state.optitrack_pos.y;
-
-  // Passing obstacle coordinates to other thread via Abi.
-  AbiSendMsgGROUP_10_OBSTACLE_DETECTION(GROUP_10_OBSTACLE_DETECTION_ID, x, y);
-
   return img;
 }
 
-void group_10_obstacle_detector_init(void)
-{
+void group_10_obstacle_detector_init(void) {
 #ifdef GROUP_10_OBSTACLE_DETECTOR_CAMERA
   cv_add_to_device(&GROUP_10_OBSTACLE_DETECTOR_CAMERA, obstacle_detector, GROUP_10_OBSTACLE_DETECTOR_FPS, 0);
 #endif
 }
 
-void group_10_obstacle_detector_periodic(void)
-{
-
-}
+void group_10_obstacle_detector_periodic(void) {}
